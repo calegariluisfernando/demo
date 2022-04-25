b0d4103cfc5b04afd4bc7ef3287962bfb6259514
Luis Fernando Calegari <luis.fernando@bevicred.com.br>
2022-04-25 17:37:31 -0300

    Adicionando provisionamento de servidores multcloud

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,14 @@
 cd iac
 ls 
 cp google.credential.file.json /tmp
-cat google.credential.file.json
-cat credentials.tfrc.json
+
+sed -i -e 's|${GOOGLE_PROJECT_KEY}|'"$GOOGLE_PROJECT_KEY"'|g' /tmp/google.credential.file.json
+sed -i -e 's|${GOOGLE_PRIVATE_KEY_ID}|'"$GOOGLE_PRIVATE_KEY_ID"'|g' /tmp/google.credential.file.json
+sed -i -e 's|${GOOGLE_PRIVATE_KEY}|'"$GOOGLE_PRIVATE_KEY"'|g' /tmp/google.credential.file.json
+sed -i -e 's|${GOOGLE_CLIENT_EMAIL}|'"$GOOGLE_CLIENT_EMAIL"'|g' /tmp/google.credential.file.json
+sed -i -e 's|${GOOGLE_CLIENT_ID}|'"$GOOGLE_CLIENT_ID"'|g' /tmp/google.credential.file.json
+sed -i -e 's|${GOOGLE_AUTH_URI}|'"$GOOGLE_AUTH_URI"'|g' /tmp/google.credential.file.json
+sed -i -e 's|${GOOGLE_URI_TOKEN}|'"$GOOGLE_URI_TOKEN"'|g' /tmp/google.credential.file.json
+sed -i -e 's|${GOOGLE_CERT_URL}|'"$GOOGLE_CERT_URL"'|g' /tmp/google.credential.file.json
+
+cat /tmp/google.credential.file.json
