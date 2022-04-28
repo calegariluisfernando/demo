5437b1262d6e32315581ef6c9c726cb0beab95b5
Luis Fernando Calegari <luis.fernando@bevicred.com.br>
2022-04-28 09:18:39 -0300

    Adicionado Máquina em Tokyo

--- iac/terraform.tf
+++ iac/terraform.tf
@@ -105,6 +105,42 @@ resource "linode_instance" "cluster-worker" {
   }
 }
 
+resource "linode_instance" "cluster-worker" {
+  label           = "cluster-worker"
+  image           = "linode/debian10"
+  region          = "ap-northeast"
+  type            = "g6-standard-2"
+  authorized_keys = [data.linode_sshkey.default.ssh_key]
+  depends_on      = [digitalocean_droplet.cluster-manager]
+
+  provisioner "remote-exec" {
+    inline = [
+      "hostnamectl set-hostname cluster-worker",
+      "apt -y update",
+      "sleep 5",
+      "apt -y upgrade",
+      "apt -y install curl wget htop unzip dnsutils",
+      "export K3S_URL=https://${digitalocean_droplet.cluster-manager.ipv4_address}:6443",
+      "export K3S_TOKEN=${var.k3s_token}",
+      "curl -sfL https://get.k3s.io | sh -"#,
+      #"export DD_AGENT_MAJOR_VERSION=7",
+      #"export DD_API_KEY=${var.datadog_agent_key}",
+      #"export DD_SITE=datadoghq.com",
+      #"curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh -o install_script.sh",
+      #"chmod +x ./install_script.sh",
+      #"./install_script.sh"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "root"
+      agent       = false
+      private_key = var.linode_ssh_key
+      host        = self.ip_address
+    }
+  }
+}
+
 output "cluster-manager-ip" {
   value = digitalocean_droplet.cluster-manager.ipv4_address
 }
